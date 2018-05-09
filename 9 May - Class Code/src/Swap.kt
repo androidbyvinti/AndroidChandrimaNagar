6c57e6e17fa793516c3f8fc5e46799bc0dd816d0
androidbyvinti <32050504+androidbyvinti@users.noreply.github.com>
2018-05-09 16:25:10 +0530

    Add files via upload

---
+++ 9 May - Class Code/src/Swap.kt
@@ -0,0 +1,62 @@
+// --> entry point --> void main()
+
+
+// void main(){}
+
+
+// fun --> function
+// main --> name of function
+
+// void print(value : int){}
+
+// main(String args[]){...} // OOPS --> class() and object
+
+
+// wrapper of Java
+// int value[]
+// Array<String> args
+
+fun main(value: Array<String>) {
+
+    //variable declaration
+
+    // char name[25] = "Ram"
+
+    // String --> char[20]
+
+    //int name = "ram"
+
+    var first  = 12 //double
+    var second = 89
+
+    // without third var and with using
+
+    // first --> second
+    // second --> first
+
+    //also --> predefined lamda function in kotlin -->
+            // also --> predefined function in kotlin --> {}
+    first = second.also { second = first }
+
+    // printf("%d", )
+    // cout<<
+
+    println(first) // print() --> println(first + "\n")
+    println(second)
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
